99adef3962ffe85e1bd3cae2124d7a75d027033f
matthewgraca <mgraca@cpp.edu>
2024-01-12 20:11:43 -0800

    have pi cam handle copy to pi server

--- get_video.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -z "$SSH_AUTH_SOCK" ]
-then
-  export SSH_AUTH_SOCK=/run/user/1000/keyring/ssh
-fi
-rsync --progress --remove-source-files -u pi@<remote_ip>:~/PorchFootage/*.mkv .
--- take_video.sh
+++ take_video.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-# prevent rpicam-vid command from overlapping 
-LOCK="fetch.lock"
-while [ -f "$LOCK" ]
+# prevent rpicam-vid from running concurrently
+lock="fetch.lock"
+while [ -f "$lock" ]
 do
   echo "Another instance of $0 is running, waiting 5 seconds..."
   sleep 5
 done
-touch $LOCK 
-trap "rm $LOCK" EXIT # if the script ends for any reason, remove lock
+touch $lock
+trap "rm $lock" EXIT # if the script ends for any reason, remove lock
 
 # write video
-current_time=$(date +"%FT%T")
-rpicam-vid -t 600000 --width 1920 --height 1080 --save-pts ${current_time}_pts.txt -o ${current_time}_raw.h264
+curr_time=$(date +"%FT%T")
+rpicam-vid -t 600000 --width 1920 --height 1080 --save-pts ${curr_time}_pts.txt -o ${curr_time}_raw.h264
+mv ${curr_time}_pts.txt ${curr_time}_raw.h264 CompleteVideos
+rm $lock
 
-# convert to .mkv, then clean up
-mkvmerge -o $current_time.mkv --timecodes 0:${current_time}_pts.txt ${current_time}_raw.h264
-rm ${current_time}_pts.txt && rm ${current_time}_raw.h264
-
+# convert to .mkv, clean up, and send to server
+cd CompleteVideos
+if [ -z "$SSH_AUTH_SOCK" ]
+then
+  export SSH_AUTH_SOCK=/run/user/1000/keyring/ssh
+fi
+mkvmerge -o ${curr_time}.mkv --timecodes 0:${curr_time}_pts.txt ${curr_time}_raw.h264
+rm ${curr_time}_pts.txt && rm ${curr_time}_raw.h264
+rsync --progress --remove-source-files ./${curr_time}.mkv pi@<pi_camera_ip>:/media/pi/c64ba661-973c-419d-a700-b42681257bd4
